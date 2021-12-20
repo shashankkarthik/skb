6e089331515d37b8642fc91924bac7d539a2c40a
Shashank Karthikeyan <shashankkarthikeyan1234@gmail.com>
2021-12-20 01:50:11 -0500

    Priming Landing

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>SKB</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Soy Koi & Bokchoy!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Our site is still under construction but check back soon!
         </p>
       </main>
 
